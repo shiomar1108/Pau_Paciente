7f8e6fede44b539086b0ec3d807c621b1fcd20e7
Shiomar Salazar <shiomarsa2@gmail.com>
2019-05-27 22:59:50 -0500

    Added Format header new file

--- main.py
+++ main.py
@@ -10,5 +10,6 @@ if exists:
 	f = open(ruta,"a")
 else:
 	f = open(ruta,"w")
+	f.write("Edad \t Talla \t Peso \t IMC \t% De Grasa \t% De Masa Muscular \t % De Grasa Vicesal \t Edad Metabolica \t C. Pecho \t C. Cintura \t C. Cadera \t C. Brazo Rep \t C. Brazo Cont \t C. Pierna \t % De Agua \t Glucosa \t PA \t Observaciones")
 
 print ("\n DONE \n")
